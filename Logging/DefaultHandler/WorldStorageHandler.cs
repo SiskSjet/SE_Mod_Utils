abfaca26cdd0f66bb9f222559958c9251a40e726
Sisk S'jet <sisk.sjet@gmail.com>
2018-07-23 07:10:44 +0200

    chore(Logging): implement an buffer check to determine if we need flush

--- Logging/DefaultHandler/WorldStorageHandler.cs
+++ Logging/DefaultHandler/WorldStorageHandler.cs
@@ -8,11 +8,13 @@ namespace Sisk.Utils.Logging.DefaultHandler {
         private readonly string _fileName;
         private readonly Formatter _formatter;
         private TextWriter _logWriter;
+        private readonly int _bufferSize;
         public WorldStorageHandler(string fileName, LogEventLevel level = LogEventLevel.All) : this(fileName, LogEvent.DefaultFormatter, level) { }
 
-        public WorldStorageHandler(string fileName, Formatter formatter, LogEventLevel level = LogEventLevel.All) : base(level) {
+        public WorldStorageHandler(string fileName, Formatter formatter, LogEventLevel level = LogEventLevel.All, int bufferSize = 50) : base(level) {
             _fileName = fileName;
             _formatter = formatter;
+            _bufferSize = bufferSize;
         }
 
         /// <inheritdoc />
@@ -30,7 +32,9 @@ namespace Sisk.Utils.Logging.DefaultHandler {
             lock (_cache) {
                 _cache.Enqueue(logEvent);
 
-                Flush();
+                if (_cache.Count > _bufferSize) {
+                    Flush();
+                }
             }
         }
 
