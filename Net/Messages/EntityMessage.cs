d4b4d2b02794b2c4e8a585affed1a6be88fe9b2e
Sisk S'jet <sisk.sjet@gmail.com>
2018-08-12 10:44:58 +0200

    feat(Net): add a network handler

---
+++ Net/Messages/EntityMessage.cs
@@ -0,0 +1,17 @@
+﻿using ProtoBuf;
+using Sandbox.ModAPI;
+using Sisk.Utils.Net.Wrapper;
+
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace Sisk.Utils.Net.Messages {
+    [ProtoContract]
+    internal class EntityMessage : IMessage {
+        [ProtoMember(1)]
+        public EntityMessageWrapper Wrapper { get; set; }
+
+        public byte[] Serialze() {
+            return MyAPIGateway.Utilities.SerializeToBinary(this);
+        }
+    }
+}
