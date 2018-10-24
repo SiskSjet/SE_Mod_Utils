8d86ec0a618580d116c5bcbe1cae6efad559149f
Sisk S'jet <sisk.sjet@gmail.com>
2018-10-24 07:53:12 +0200

    feat(Localization): add localization lib, old method only work offline

---
+++ Localization/Lang.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VRage;
+
+namespace Sisk.Utils.Localization {
+    public class Lang {
+        private static readonly Dictionary<MyLanguagesEnum, IDictionary<string, string>> Map = new Dictionary<MyLanguagesEnum, IDictionary<string, string>> {
+            { MyLanguagesEnum.English, English }
+        };
+
+        private static IDictionary<string, string> English => new Dictionary<string, string>();
+
+        /// <summary>
+        ///     Add a new language dictionary.
+        /// </summary>
+        /// <param name="language">The language of the dictionary.</param>
+        /// <param name="dictionary">The dictionary containing the translations.</param>
+        public static void Add(MyLanguagesEnum language, IDictionary<string, string> dictionary) {
+            if (!Map.ContainsKey(language)) {
+                Map.Add(language, dictionary);
+            } else {
+                Map[language] = dictionary;
+            }
+        }
+
+        /// <summary>
+        ///     Check if dictionary for given language exists.
+        /// </summary>
+        /// <param name="language">The language key.</param>
+        /// <returns>Return true if dictionary exists and is not empty.</returns>
+        public static bool Contains(MyLanguagesEnum language) {
+            return Map.ContainsKey(language) && Map[language].Any();
+        }
+
+        /// <summary>
+        ///     Return the dictionary for given language.
+        /// </summary>
+        /// <param name="language">The language key.</param>
+        /// <returns>Return a dictionary with translation for given language.</returns>
+        public static IDictionary<string, string> Get(MyLanguagesEnum language) {
+            return Map[language];
+        }
+    }
+}
---
+++ Localization/Texts.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Utils;
+
+// ReSharper disable InlineOutVariableDeclaration
+
+namespace Sisk.Utils.Localization {
+    public static class Texts {
+        public const string LOCALIZATION_TAG = "LOC:";
+        public const string LOCALIZATION_TAG_GENERAL = "LOCG:";
+
+        private static readonly Dictionary<MyStringId, StringBuilder> StringBuilders = new Dictionary<MyStringId, StringBuilder>(MyStringId.Comparer);
+        private static readonly Dictionary<MyStringId, string> Strings = new Dictionary<MyStringId, string>(MyStringId.Comparer);
+
+        public static StringBuilder AppendFormat(this StringBuilder stringBuilder, MyStringId textEnum, object arg0) {
+            return stringBuilder.AppendFormat(GetString(textEnum), arg0);
+        }
+
+        public static void Clear() {
+            Strings.Clear();
+            StringBuilders.Clear();
+            Strings[new MyStringId()] = "";
+            StringBuilders[new MyStringId()] = new StringBuilder();
+        }
+
+        public static bool Exists(MyStringId id) {
+            return Strings.ContainsKey(id);
+        }
+
+        public static StringBuilder Get(MyStringId id) {
+            StringBuilder sb;
+            if (!StringBuilders.TryGetValue(id, out sb)) {
+                sb = MyTexts.Get(id);
+            }
+
+            return sb;
+        }
+
+        public static string GetString(MyStringId id) {
+            string str;
+            if (!Strings.TryGetValue(id, out str)) {
+                str = MyTexts.GetString(id);
+            }
+
+            return str;
+        }
+
+        public static string GetString(string keyString) {
+            return GetString(MyStringId.GetOrCompute(keyString));
+        }
+
+        public static bool IsTagged(string text, int position, string tag) {
+            return !tag.Where((t, index) => text[position + index] != (int) t).Any();
+        }
+
+        public static void LoadSupportedLanguages(HashSet<MyLanguagesEnum> outSupportedLanguages) {
+            foreach (var value in Enum.GetValues(typeof(MyLanguagesEnum)).Cast<MyLanguagesEnum>()) {
+                if (Lang.Contains(value)) {
+                    outSupportedLanguages.Add(value);
+                }
+            }
+        }
+
+        public static void LoadTexts(MyLanguagesEnum language = MyLanguagesEnum.English) {
+            var dict = Lang.Get(language);
+            if (dict != null && dict.Any()) {
+                PatchTexts(dict);
+            }
+        }
+
+        public static void PatchTexts(IDictionary<string, string> resources) {
+            foreach (var entry in resources) {
+                var key = entry.Key;
+                var str = entry.Value;
+                if (key != null && str != null) {
+                    var id = MyStringId.GetOrCompute(key);
+                    Strings[id] = str;
+                    StringBuilders[id] = new StringBuilder(str);
+                }
+            }
+        }
+
+        public static string SubstituteTexts(string text) {
+            if (!text.StartsWith("{") || !text.EndsWith("}")) {
+                return text;
+            }
+
+            if (IsTagged(text, 1, LOCALIZATION_TAG)) {
+                var startIndex = LOCALIZATION_TAG.Length + 1;
+                var stringBuilder = Get(MyStringId.GetOrCompute(text.Substring(startIndex, text.Length - startIndex - 1)));
+                if (stringBuilder != null) {
+                    return stringBuilder.ToString();
+                }
+            } else if (IsTagged(text, 1, LOCALIZATION_TAG_GENERAL)) {
+                var startIndex = LOCALIZATION_TAG_GENERAL.Length + 1;
+                var stringBuilder = Get(MyStringId.GetOrCompute(text.Substring(startIndex, text.Length - startIndex - 1)));
+                if (stringBuilder != null) {
+                    return stringBuilder.ToString();
+                }
+            }
+
+            return text;
+        }
+
+        public static string SubstituteTextsDirect(string text) {
+            if (text[0] != '{' || text[text.Length - 1] != '}' || !IsTagged(text, 1, LOCALIZATION_TAG)) {
+                return text;
+            }
+
+            var startIndex = LOCALIZATION_TAG.Length + 1;
+            var stringBuilder = Get(MyStringId.GetOrCompute(text.Substring(startIndex, text.Length - startIndex - 1)));
+            if (stringBuilder != null) {
+                return stringBuilder.ToString();
+            }
+
+            return text;
+        }
+
+        public static string TrySubstitute(string input) {
+            var orCompute = MyStringId.GetOrCompute(input);
+            StringBuilder sb;
+            return !StringBuilders.TryGetValue(orCompute, out sb) ? input : sb.ToString();
+        }
+    }
+}
